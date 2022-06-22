8045e86c63894e54c8b8011ad5dd473690edba2b
François <Schmitt.francois.1699@gmail.com>
2022-06-22 18:45:15 +0200

    end of project

--- src/pages/Accommodation/index.jsx
+++ src/pages/Accommodation/index.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, UseNavigate, Navigate } from 'react-router-dom';
 import logements from '../../data/logements.json';
 import Dropdown from '../../components/Dropdown';
 import InfoUserCard from '../../components/InfoUserCard';
@@ -8,82 +7,101 @@ import InfoAccommodation from '../../components/InfoAccommodation';
 import './index.css';
 import Stars from '../../components/Stars';
 import Slider from '../../components/Slider';
-
-function getAccomodation(id) {
-      return logements.filter((logement) => logement.id === id)[0];
-}
+import { useEffect } from 'react';
 
 function Accomodation() {
-      const { id } = useParams();
-      const navigate = useNavigate();
-      const accommodation = getAccomodation(id);
-
-      useEffect(() => {
-            // if (id === 'c67ab8a7') {
-            //       console.log(id + "ceci est l'id que je recherche ");
-            //       console.log(accommodation.id + "ceci est l'id de mon fichier json");
-            //       console.log(accommodation); //renvoie le tableau de donnée en fonction de l'id trouvé
-            // }
-            // else  {
-            //       navigate('/404');
-            // }
-      }, []);
+      const id = useParams();
+      // const navigate = useNavigate();
+      const accommodation = logements.find((logement) => logement.id === id.id);
+      // useEffect(()=>{
+      //       const navigate = UseNavigate();
+      //       if(!accommodation)
+      //       {
+      //             navigate("/404")
+      //       }
+      // },[])
 
       return (
-            <section className="accommodation-section">
-                  <Slider pictures={accommodation.pictures} />
-                  <div className="accommodation">
-                        <div className="container-accommodation">
-                              <InfoAccommodation
-                                    title={accommodation.title}
-                                    infoTitle={accommodation.location}
-                              />
-                              <div className="tagsSection">
-                                    {accommodation.tags.map((tag, index) => (
-                                          <Tag
-                                                key={`tag-${index}`}
-                                                tagName={tag}
+            <>
+                  {accommodation ? (
+                        <section className="accommodation-section">
+                              <Slider pictures={accommodation.pictures} />
+                              <div className="accommodation">
+                                    <div className="container-accommodation">
+                                          <InfoAccommodation
+                                                title={accommodation.title}
+                                                infoTitle={
+                                                      accommodation.location
+                                                }
                                           />
-                                    ))}
+                                          <div className="tagsSection">
+                                                {accommodation.tags.map(
+                                                      (tag, index) => (
+                                                            <Tag
+                                                                  key={`tag-${index}`}
+                                                                  tagName={tag}
+                                                            />
+                                                      )
+                                                )}
+                                          </div>
+                                    </div>
+                                    <div className="card-rating">
+                                          <InfoUserCard
+                                                nameUser={
+                                                      accommodation.host.name
+                                                }
+                                                imageUser={
+                                                      accommodation.host.picture
+                                                }
+                                                tags={accommodation.tags}
+                                          />
+                                          <Stars
+                                                EventStars={
+                                                      accommodation.rating
+                                                }
+                                          />
+                                    </div>
                               </div>
-                        </div>
-                        <div className="card-rating">
-                              <InfoUserCard
-                                    nameUser={accommodation.host.name}
-                                    imageUser={accommodation.host.picture}
-                                    tags={accommodation.tags}
-                              />
-                              <Stars EventStars={accommodation.rating} />
-                        </div>
-                  </div>
-                  <div className="accommodation-dropdown-content">
-                        <div className="accomodationDropdowns">
-                              <Dropdown
-                                    title="Description"
-                                    content={accommodation.description}
-                                    className="lolasa"
-                              />
+                              <div className="accommodation-dropdown-content">
+                                    <div className="accomodationDropdowns">
+                                          <Dropdown
+                                                title="Description"
+                                                content={
+                                                      accommodation.description
+                                                }
+                                                className="lolasa"
+                                          />
 
-                              <Dropdown
-                                    title="Équipements"
-                                    content={accommodation.equipments.map(
-                                          (equipment, index) => (
-                                                <ul
-                                                      key={`equipment-${index}`}
-                                                      className="accommodation-content-equipment"
-                                                >
-                                                      <li>{equipment}</li>
-                                                </ul>
-                                          )
-                                    )}
-                              />
-                        </div>
-                  </div>
-            </section>
+                                          <Dropdown
+                                                title="Équipements"
+                                                content={accommodation.equipments.map(
+                                                      (equipment, index) => (
+                                                            <ul
+                                                                  key={`equipment-${index}`}
+                                                                  className="accommodation-content-equipment"
+                                                            >
+                                                                  <li>
+                                                                        {
+                                                                              equipment
+                                                                        }
+                                                                  </li>
+                                                            </ul>
+                                                      )
+                                                )}
+                                          />
+                                    </div>
+                              </div>
+                        </section>
+                  ) : (
+                        <Navigate replace to="/404" />
+                  )}
+            </>
       );
 }
+
 export default Accomodation;
 
+// useEffect(()=>{
 // if (!id == newId.id){
 //     console.log(newId.id + "lol")
 //     navigate("/404")
